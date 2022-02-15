69b3c8094c05a504a4f79325e96089fe3d35992f
fxb <931935931@qq.com>
2022-02-15 12:36:57 +0800

    new

--- _递归与递推/_002_92递归实现指数型枚举.cpp
+++ _递归与递推/_002_92递归实现指数型枚举.cpp
@@ -3,30 +3,34 @@
  * @Author: fengxb
  * @Date: 2022-02-15 11:16:40
  * @LastEditor: fengxb
- * @LastEditTime: 2022-02-15 12:15:21
+ * @LastEditTime: 2022-02-15 12:35:18
  */
 #include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 const int N = 16;
 
 int n;
 int st[N]; //状态，记录每个位置当前的状态，0表示还没考虑，1表示选他，2表示不选它
+vector<vector<int>> ways; //记录方案
 
 void dfs(int u){
     if(u > n)
     {
-        //输出结果
+        //当前方案
+        vector<int> way;
         for (int i = 1; i <= n; i++)
         {
             if(st[i] == 1){
-                printf("%d ",i);
+                //记录方案
+                way.push_back(i);
             }
         }
-        printf("\n");
+        ways.push_back(way);
         return;
     }
 
@@ -43,6 +47,14 @@ int main()
 {
     cin >> n;
     dfs(1);
+
+    //输出方案
+    for(int i = 0; i < ways.size(); i++){
+        for(int j = 0; j < ways[i].size(); j++){
+            printf("%d ",ways[i][j]);
+        }
+        puts("");
+    }
 
     return 0;
 }
