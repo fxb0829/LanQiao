8ac6be2f19397dc79e892bdf9436b67d2170667e
fxb <931935931@qq.com>
2022-02-17 12:27:37 +0800

    new

--- _递归与递推/_001_717斐波拉契数列.cpp
+++ _递归与递推/_001_717斐波拉契数列.cpp
@@ -3,39 +3,24 @@
  * @Author: fengxb
  * @Date: 2022-02-15 09:57:56
  * @LastEditor: fengxb
- * @LastEditTime: 2022-02-15 11:08:16
+ * @LastEditTime: 2022-02-17 12:23:11
  */
 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-//斐波拉契数列--递归
-int febo(int n){
-    if(n == 0)return 0;
-    if(n == 1)return 1;
-    return febo(n-1)+febo(n-2);
-}
 
 int main(){
     int n;
     cin >> n;
-    //递归版本
-    // for(int i = 0; i < n; i++){
-    //     cout << febo(i) << " ";
-    // }
-
-    //非递归版本
     int x = 0,y = 1;
-    for (int i = 0; i < n; i++){
-        if(i <= 1){
-            cout << i << " ";
-        }else{
-            int temp = x+y;
-            cout << temp << " ";
-            x = y;
-            y = temp;
-        }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << x << " ";
+        int tmp = x + y;
+        x = y;
+        y = tmp;
     }
     
     return 0;
