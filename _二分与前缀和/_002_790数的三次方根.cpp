b65026f30e9c41da50d8611c5dbd29a0debff994
fxb <931935931@qq.com>
2022-02-18 18:12:14 +0800

    new

--- _二分与前缀和/_002_790数的三次方根.cpp
+++ _二分与前缀和/_002_790数的三次方根.cpp
@@ -3,7 +3,7 @@
  * @Author: fengxb
  * @Date: 2022-02-18 17:31:09
  * @LastEditor: fengxb
- * @LastEditTime: 2022-02-18 17:45:23
+ * @LastEditTime: 2022-02-18 18:12:10
  */
 #include <cstdio>
 #include <cstring>
@@ -14,5 +14,16 @@ using namespace std;
 
 int main()
 {
-    
+    double x;
+    cin >> x;
+    double l = -10000,r = 10000;
+    while (r-l > 1e-8)
+    {
+        double mid = (l+r)/2;
+        if(mid*mid*mid >= x) r = mid;
+        else l = mid;
+    }
+    printf("%lf\n",l);
+
+    return 0;
 }
