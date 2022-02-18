6c95722bda520bc987759161f445b90bbdb1cb16
fxb <931935931@qq.com>
2022-02-18 17:45:30 +0800

    new

---
+++ _二分与前缀和/_002_790数的三次方根.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: fengxb
+ * @Date: 2022-02-18 17:31:09
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-02-18 17:45:23
+ */
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    
+}
