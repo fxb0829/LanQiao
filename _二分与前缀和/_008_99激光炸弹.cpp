4971dd0c41fa7bd73e25e94cda658995090a220f
fxb <931935931@qq.com>
2022-02-21 20:29:44 +0800

    new

---
+++ _二分与前缀和/_008_99激光炸弹.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Author: fengxb
+ * @Date: 2022-02-21 20:24:31
+ * @LastEditor: fengxb
+ * @LastEditTime: 2022-02-21 20:29:38
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 10010;
+
