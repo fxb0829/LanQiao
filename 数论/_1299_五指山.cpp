3604af82f68da06dbe68f834a2bebc0cfc3b7725
fxb <931935931@qq.com>
2022-04-15 23:12:28 +0800

    20220415

--- 数论/_1299_五指山.cpp
+++ 数论/_1299_五指山.cpp
@@ -17,22 +17,19 @@ LL exgcd(LL a,LL b,LL &x, LL &y){
 }
 
 int main(){
-    // int T;
-    // scanf("%d",&T);
-    // while(T--){
-    //     LL n,d,x,y,a,b;
-    //     scanf("%lld%lld%lld%lld",&n,&d,&x,&y);
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        LL n,d,x,y,a,b;
+        scanf("%lld%lld%lld%lld",&n,&d,&x,&y);
 
-    //     int gcd = exgcd(n,d,a,b);
-    //     if((y-x) % gcd) puts("Impossible");
-    //     else{
-    //         b *= (y-x) / gcd;
-    //         n /= gcd;
-    //         printf("%lld\n",(b % n + n)%n);
-    //     }
-    // }
-    LL a,b;
-    cout << exgcd(10,6,a,b) << endl;
-    cout << a << " " << b << endl;
+        int gcd = exgcd(n,d,a,b);
+        if((y-x) % gcd) puts("Impossible");
+        else{
+            b *= (y-x) / gcd;
+            n /= gcd;
+            printf("%lld\n",(b % n + n)%n);
+        }
+    }
     return 0;
 }
